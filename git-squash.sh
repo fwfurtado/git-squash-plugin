86cddc7d65ba87ddb69c1d868f328198984e1629
Fernando Willian <feh.wilinando@gmail.com>
2018-08-27 22:39:26 -0300

    Added feature to squash by diff between current branch and specific branch

--- git-squash.sh
+++ git-squash.sh
@@ -9,7 +9,7 @@ usage() {
 	echo
 	
 	echo "\tgit squash --commit <commit>"
-	echo "\tgit squash --diff [<source-branch>] <target-branch>"
+	echo "\tgit squash --diff <target-branch>"
 
 	echo "DESCRIPTION:"
 	echo
@@ -24,9 +24,8 @@ usage() {
 	echo "\t\tReset all changes till the <commit> to stage"
 	echo
 
-	echo "\t-d, --diff [<source-branch>] <target-branch>"
-	echo "\t\tReset all changes between <source-branch> and <target-branch> to stage"
-	echo "\t\tIf the <source-branch> isn't specified, used HEAD for comparison."
+	echo "\t-d, --diff <target-branch>"
+	echo "\t\tReset all changes between HEAD and <target-branch> to stage"
 	echo
 
 	echo "EAMPLE:"
@@ -34,8 +33,7 @@ usage() {
 	
 	echo "\tgit squash --commit HEAD~1"
 	echo "\tgit squash --commit 0987208108265812"
-	echo "\tgit squash --diff feature release"
-	echo "\tgit squash --diff release"
+	echo "\tgit squash --diff master"
 	echo
 }
 
@@ -54,7 +52,7 @@ main(){
 			squash_by_commit "$@";
 			;;
 		"-d"|"--diff")
-			squash_by_diff
+			squash_by_diff "$@";
 			;;
 		*)
 			usage; exit 1
@@ -70,9 +68,33 @@ squash_by_commit() {
 		usage; exit 1
 	fi
 
-	local commit_expression="$@"
+	local commit_expression="$1"
 
 	git reset --soft $commit_expression
+
+	exit 0
+}
+
+squash_by_diff() {
+	if [ "$#" -lt 1 ]; then
+		echo "Missing argument <target-branch>"
+		echo
+		usage; exit 1
+	fi
+
+	local source_branch="HEAD"
+	local target_branch="$1"
+	local timestamp=$(date +%s)
+	
+	local source_commits_file=$(echo "${TMPDIR}${timestamp}_${source_branch}.log")
+	local target_commits_file=$(echo "${TMPDIR}${timestamp}_${target_branch}.log")
+
+	git rev-list $source_branch > $source_commits_file
+	git rev-list $target_branch > $target_commits_file
+
+	local total_of_commits="$(diff $source_commits_file $target_commits_file | grep "<" | sed -n \$=)"
+
+	git reset --soft HEAD~$total_of_commits
 
 	exit 0
 }
