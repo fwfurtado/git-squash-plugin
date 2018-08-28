18e1fc3aae4db25a462a94d45a238b4dce01e25f
Fernando Willian <feh.wilinando@gmail.com>
2018-08-28 13:42:31 -0300

    Create plugin directory if not exists

--- .installation.sh
+++ .installation.sh
@@ -4,6 +4,8 @@
 install () {
 	local plugin_path=$HOME/.git-extensions/git-squash-plugin
 	
+	mkdir -p $plutin_path > /dev/null
+
 	pushd $(pwd) > /dev/null
 	
 	git clone https://github.com/feh-wilinando/git-squash-plugin.git $plugin_path
